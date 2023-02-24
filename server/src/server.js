2289a352b0ca4b9ffa55be18e85a63e5fced29f1
Claudette Alexander <claudette.alexander@developersinstitute.co.nz>
2023-02-25 07:49:06 +0900

    test env

--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,7 @@ const port = process.env.PORT || 5001;
 const app = require("./app");
 
 const mongoDbUri = process.env.MONGO_URI || "mongodb://localhost:27017/mongo";
-mongoose.connect(mongoDbUri, {
-  dbName: process.env.DB_Name,
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
-});
+mongoose.connect(mongoDbUri);
 
 app.listen(port, () => {
   console.log(`API server started at ${port}`);
